4ce660b337a21e219b8c6a10521a59c1f6bc3117
AxelMello1998 <alessandromelotti1998@gmail.com>
2025-05-15 20:00:00 +0200

    init

---
+++ src/test/java/com/progettomedusa/player_sheets_service/PlayerSheetsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progettomedusa.player_sheets_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayerSheetsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
